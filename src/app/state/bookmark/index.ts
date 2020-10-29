146156128d471f869d4b72f1613e87e4a7b28ec8
Mykyta Korba <no.korba@gmail.com>
2020-10-29 15:45:29 +0200

    More refactoring

--- src/app/state/bookmark/index.ts
+++ src/app/state/bookmark/index.ts
@@ -1,5 +1,3 @@
-export * from './bookmark.reducers';
-export * from './bookmark.state';
 import * as BookmarkActions from './bookmark.actions';
 import * as BookmarkState from './bookmark.state';
 import * as BookmarkReducer from './bookmark.reducers';
