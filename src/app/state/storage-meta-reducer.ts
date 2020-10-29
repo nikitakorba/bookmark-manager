de1d30f21ea78a642144e67a8e83e6fe4f446c2a
Mykyta Korba <no.korba@gmail.com>
2020-10-29 15:45:29 +0200

    More refactoring

--- src/app/state/storage-meta-reducer.ts
+++ src/app/state/storage-meta-reducer.ts
@@ -1,6 +1,7 @@
 import { Action, ActionReducer } from '@ngrx/store';
+import { ApplicationState } from '../app.state';
 
-const localStorageKey: string = 'gl_bookmarks_state';
+const localStorageKey: string = 'bookmarks_state';
 
 function setSavedState(state: any) {
   localStorage.setItem(localStorageKey, JSON.stringify(state));
@@ -11,10 +12,10 @@ function getSavedState(): any {
 }
 
 
-export function storageMetaReducer<S, A extends Action = Action>(reducer: ActionReducer<S, A>) {
+export function storageMetaReducer<ApplicationState, A extends Action = Action>(reducer: ActionReducer<ApplicationState, A>) {
   let appInit = true;
-  return function (state: S, action: A): S {
-    const nextState = reducer(state, action);
+  return function (state: ApplicationState, action: A): ApplicationState {
+    const nextState: ApplicationState = reducer(state, action);
     if (appInit) {
       appInit = false;
       return {...nextState, ...getSavedState()};
